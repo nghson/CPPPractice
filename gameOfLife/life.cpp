06869223e8359d6a412ffe23fee759e67c4cdf2e
nghson <son.hngy@gmail.com>
2023-07-18 02:33:13 +0300

    func to make random grid

--- gameOfLife/life.cpp
+++ gameOfLife/life.cpp
@@ -1,32 +1,71 @@
 #include <iostream>
 #include <vector>
+#include <string>
+#include <random>
 
-using std::cout, std::vector;
+using std::cout, std::vector, std::string;
+
+struct Cell {
+        char mark;
+        unsigned int age;
+};
 
 void greet();
-void printMap(vector<vector<char>> &grid);
+void parseFile(string fileName, vector<vector<Cell>> &grid);
+vector<vector<Cell>> makeRandomGrid(int x, int y);
+void printMap(const vector<vector<Cell>> &grid);
+vector<vector<Cell>> advance(const vector<vector<Cell>> &grid);
 
 
 int main()
 {
-        vector<vector<char>> grid(3, vector<char>(4, '-'));
+        vector<vector<Cell>> grid = makeRandomGrid(5, 5);
         greet();
-        printMap(grid);
+        for (int i = 0; i < 3; i++) {
+                grid = advance(grid);
+                printMap(grid);
+        }
 }
 
 void greet()
 {
-        cout << "Welcome to the Game of life\n";
+        cout << "Welcome to the Game of life\n\n";
 }
 
-void printMap(vector<vector<char>> &grid)
+void printMap(const vector<vector<Cell>> &grid)
 {
         int x = grid.size();
         int y = grid[0].size(); 
         for (int i = 0; i < x; ++i) {
                 for (int j = 0; j < y; ++j) {
-                        cout << grid[i][j];
+                        Cell cell = grid[i][j];
+                        cout << cell.mark;
                 }
                 cout << '\n';
         }
+}
+
+void parseFile(string filename, vector<vector<Cell>> &grid)
+{
+
+}
+
+vector<vector<Cell>> makeRandomGrid(int x, int y)
+{
+        vector<vector<Cell>> grid(x, vector<Cell>(y, {'-', 0}));
+        std::default_random_engine e;
+        std::uniform_real_distribution<double> u(0, 1);
+        for (int i = 0; i < x; ++i) {
+                for (int j = 0; j < y; ++j) {
+                        if (u(e) > 0.5) {
+                                grid[i][j].mark = 'X';
+                        }
+                }
+        }
+        return grid;
+}
+
+vector<vector<Cell>> advance(const vector<vector<Cell>> &grid)
+{
+
 }
