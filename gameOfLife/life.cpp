5f3dd3ae7ba8d1e3852cf1d0b6bf258b3e835cef
nghson <son.hngy@gmail.com>
2023-07-19 22:33:32 +0300

    added nextGen

--- gameOfLife/life.cpp
+++ gameOfLife/life.cpp
@@ -13,30 +13,39 @@ struct Cell {
 void greet();
 vector<vector<Cell>> parseFile(string fileName);
 vector<Cell> makeRandomGrid(int x, int y);
-vector<Cell> advance(const vector<Cell> &grid);
+vector<Cell> advance(const vector<Cell> &grid, int x, int y);
 void printMap(const vector<Cell> &grid, int x, int y);
+int n_neighbors(const vector<Cell>& grid, int x, int y, int dx, int dy);
+Cell nextGen(const Cell& c, int neighbors);
 
 void testPrintGrid(void (*printer)(const vector<Cell>&, int, int))
 {
         vector<Cell> grid(3 * 3, {'-', 0});
         vector<int> points = {0, 1, 2, 3, 7};
         for (int num : points) {
-                grid[num].mark = 'X';
+                grid[num].mark = 'x';
         }
-        string expected = "XXX\nX--\n-X-";
+        string expected = "xxx\nx--\n-x-";
         cout << "Expected: " << '\n';
         cout << expected << '\n';
         cout << "Result: " << '\n';
         printer(grid, 3, 3);
 }
 
+//void testNextGen(Cell (*f)(const Cell&, int))
+//{
+//        
+//}
+
 int main()
 {
+        int x = 5;
+        int y = 5;
         vector<Cell> grid = makeRandomGrid(5, 5);
         greet();
-        printMap(grid, 5, 5);
+        printMap(grid, x, y);
         //for (int i = 0; i < 3; i++) {
-        //        grid = advance(grid);
+        //        grid = advance(grid, x, y);
         //        printMap(grid);
         //}
 }
@@ -70,14 +79,54 @@ vector<Cell> makeRandomGrid(int x, int y)
         for (int i = 0; i < x; ++i) {
                 for (int j = 0; j < y; ++j) {
                         if (u(e) > 0.5) {
-                                grid[i * y + j].mark = 'X';
+                                Cell& c = grid[i * y + j];
+                                c.mark = 'x';
+                                c.age = 1;
                         }
                 }
         }
         return grid;
 }
 
-//vector<Cell> advance(const vector<Cell> &grid)
-//{
-//
-//}
+vector<Cell> advance(const vector<Cell> &grid, int x, int y)
+{
+        vector<Cell> new_grid(x * y, {'-', 0});
+        for (int i = 0; i < x; ++i) {
+                for (int j = 0; j < x; ++j) {
+                        Cell c = grid[i * y + j];
+                        int neighbors = n_neighbors(grid, x, y, i, j);
+
+                        Cell new_c = nextGen(c, neighbors);
+                        new_grid[i * y + j] = new_c;
+                }
+        }
+
+        return new_grid;
+}
+
+int n_neighbors(const vector<Cell>& grid, int x, int y, int dx, int dy)
+{
+        int neighbors = 0;
+
+        return neighbors;
+}
+
+Cell nextGen(const Cell& c, int neighbors)
+{
+        Cell new_c = {'-', 0};
+        if (c.age >= 10) {
+                return new_c;
+        }
+
+        if (neighbors <= 1 || neighbors >= 4) {
+                return new_c;
+        } else if (neighbors == 2) {
+                new_c = c;
+                new_c.age = c.age > 0 ? c.age + 1 : c.age;
+        } else {
+                new_c.age = c.age + 1;
+                new_c.mark = c.age > 3 ? 'X' : 'x';
+        }
+
+        return new_c;
+}
