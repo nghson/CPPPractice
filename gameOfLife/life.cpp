c9b8b70b6f73ec02866ce0c84cf32cc92d76a552
nghson <son.hngy@gmail.com>
2023-07-21 23:36:29 +0300

    GOL makeRandomGrid uses ref param

--- gameOfLife/life.cpp
+++ gameOfLife/life.cpp
@@ -21,7 +21,7 @@ struct GridMap {
 
 void initMap(char source, const string& filename, int x, int y, GridMap& g_map);
 void parseFile(const string& filename, GridMap& g_map);
-vector<Cell> makeRandomGrid(int x, int y);
+void makeRandomGrid(int x, int y, vector<Cell>& grid);
 
 void advance(vector<Cell> &grid, vector<Cell>& newGrid, int x, int y);
 int n_neighbors(const vector<Cell>& grid, int x, int y, int dx, int dy);
@@ -37,23 +37,23 @@ int main()
         string filename;
         int x;
         int y;
-        //cout << "Read from (f)ile, or (r)andom? ";
-        //cin >> source;
-        //if (source == 'f') {
-        //        cout << "Grid input file name? ";
-        //        cin  >> filename;
-        //} else if (source == 'r') {
-        //        cout << "How many rows? ";
-        //        cin >> x;
-        //        cout << "How many columns? ";
-        //        cin >> y;
-        //} else {
-        //        cout << "Unknown option, try again";
-        //}
+        cout << "Read from (f)ile, or (r)andom? ";
+        cin >> source;
+        if (source == 'f') {
+                cout << "Grid input file name? ";
+                cin  >> filename;
+        } else if (source == 'r') {
+                cout << "How many rows? ";
+                cin >> x;
+                cout << "How many columns? ";
+                cin >> y;
+        } else {
+                cout << "Unknown option, try again";
+        }
 
         GridMap g_map;
-        //initMap(source, filename, x, y, g_map);
-        initMap('f', "grid.txt", x, y, g_map);
+        initMap(source, filename, x, y, g_map);
+        //initMap('f', "grid.txt", x, y, g_map);
 
         printMap(g_map.grid, g_map.x, g_map.y);
 
@@ -73,6 +73,10 @@ void initMap(char source, const string& filename, int x, int y, GridMap& g_map)
 {
         if (source == 'f') {
                 parseFile(filename, g_map);
+        } else if (source == 'r') {
+                vector<Cell> grid(x * y, {'-', 0});
+                makeRandomGrid(x, y, grid);
+                g_map = {grid, x, y};
         }
 }
 
@@ -105,7 +109,9 @@ void parseFile(const string& filename, GridMap& g_map)
                 std::getline(file, s);
                 for (int j = 0; j < y; ++j) {
                         if (s[j] == 'X') {
-                                grid[i * y + j] = Cell{'x', 1};
+                                Cell& c = grid[i * y + j];
+                                c.mark = 'x';
+                                c.age = 1;
                         }
                 }
         }
@@ -114,9 +120,8 @@ void parseFile(const string& filename, GridMap& g_map)
         file.close();
 }
 
-vector<Cell> makeRandomGrid(int x, int y)
+void makeRandomGrid(int x, int y, vector<Cell>& grid)
 {
-        vector<Cell> grid(x * y, {'-', 0});
         std::default_random_engine e;
         std::uniform_real_distribution<double> u(0, 1);
         for (int i = 0; i < x; ++i) {
@@ -128,7 +133,6 @@ vector<Cell> makeRandomGrid(int x, int y)
                         }
                 }
         }
-        return grid;
 }
 
 void advance(vector<Cell>& grid, vector<Cell>& newGrid, int x, int y)
