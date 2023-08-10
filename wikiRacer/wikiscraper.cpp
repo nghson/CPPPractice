e0b8894a94fd74d155dd5d1384437e61943f9b1e
nghson <son.hngy@gmail.com>
2023-08-10 23:53:58 +0300

    wikiscraper skeleton

---
+++ wikiRacer/wikiscraper.cpp
@@ -0,0 +1,26 @@
+#include <unordered_set>
+#include <string>
+#include "wikiscraper.h"
+
+using std::string;
+using std::unordered_set;
+
+unordered_set<string> findWikiLinks(const string& page_html)
+{
+
+}
+
+void findBegin()
+{
+
+}
+
+void findEnd()
+{
+
+}
+
+void addLink(unordered_set<string>& links)
+{
+
+}
