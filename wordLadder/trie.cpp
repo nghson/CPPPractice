d8ca42a019cab156b49217d690314a6710b820c1
nghson <son.hngy@gmail.com>
2023-08-27 23:47:40 +0300

    clean input

--- wordLadder/trie.cpp
+++ wordLadder/trie.cpp
@@ -1,9 +1,12 @@
 #include <string>
 #include <vector>
+#include <cctype>
 #include "trie.h"
 #include "Node.h"
 
 using std::string;
+
+bool clean(string& s);
 
 Trie::Trie()
 {
@@ -44,4 +47,21 @@ bool Trie::contains(const string& s)
 bool _insert(Node*& node, const string& word)
 {
 
+}
+
+bool clean(string& s)
+{
+        const size_t l = s.size();
+        size_t ind = 0;
+        for (size_t i = 0; i < l; ++i) {
+                auto c = std::tolower(s[i]);
+                if (c < 'a' || c > 'z') {
+                        return false;
+                }
+                s[ind++] = c;
+        }
+        if (ind != l) {
+                s.erase(ind, l - ind);
+        }
+        return true;
 }
